8c006ba8a0395a9b374c59e420bd79e00277f5df
Dean Howe <hideanhowe@gmail.com>
2025-12-30 22:37:49 +0000

    solution

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 // Given a positive integer, return a string with all of the integers from 1 up to, and including, the given number, in numerical order.
 
 // For example, given 5, return "12345".
+
+function sequence(n) {
+  console.log(n);
+  const arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+  console.log(arr);
+
+  return arr.join('');
+}
